b5af99646b8dd7cde9574d10c916351064f5d688
jwc780 <chengwjin32666@gmail.com>
2020-03-02 19:37:34 -0500

    improved ship shell generation

--- gameParamsToShipShells.py
+++ gameParamsToShipShells.py
@@ -31,7 +31,7 @@ with open("GameParams.json", 'r') as f:
             condenseGun[k] = v
 
 condensedDirectory = 'condensed'
-writeCondensed = True
+writeCondensed = False
 if writeCondensed:
     print('Writing Condensed Files')
     checkMakeDir(condensedDirectory)
@@ -39,50 +39,84 @@ if writeCondensed:
     writeToFile(condenseShell, condensedDirectory, 'condensendShells.json')
     writeToFile(condenseGun  , condensedDirectory, 'condensendGuns.json')
 
+def selectEssential(data):
+    targetKeys = set([
+        "bulletAirDrag", 
+        "bulletAlwaysRicochetAt",
+        "bulletDetonator", 
+        "bulletDetonatorThreshold", 
+        "bulletDiametr", 
+        "bulletKrupp", 
+        "bulletMass", 
+        "name", 
+        "bulletRicochetAt", 
+        "bulletSpeed",
+        "bulletCapNormalizeMaxAngle"
+    ])
+    output = {}
+    for target in targetKeys:
+        output[target] = data[target]
+    return output
+
 
 shipShells = {}
 shipShellData = {}
-for ship, artillery in condenseShip.items(): #Ship Art
+shipShellDataEssential = {}
+for ship, attributes in condenseShip.items(): #Ship Art
     added = False
-    for gun, gunV in artillery.items(): #Art HP
-        if type(gunV) == type(dict()):
-            for k, v in gunV.items(): #HP AL
-                #print(vv)
-                if type(v) == type(dict()):
-                    if 'ammoList' in v:
-                        if v['typeinfo']['species'] == 'Main':
-                            ammoList = set()
-                            for kvv, vvv in gunV.items():
-                                #print(type(vvv))
-                                #print(vvv)
-                                if type(vvv) == type(dict()) and 'ammoList' in vvv:
-                                    added = True
-                                    for shell in vvv['ammoList']:
-                                        ammoList.add(shell)
-                                #ammoList.add(vvv['ammoList'])
-                            
-                            #print(ammoList)
-                            if ship in shipShells:
-                                shipShellsSet = set(shipShells[ship])
-                                #print(shipShellsSet)
-                                for shell in ammoList:
-                                    shipShellsSet.add(shell)
-                                if len(shipShells[ship]) <= 2 and len(shipShellsSet) > 2:
-                                    print(ship)
-                                shipShells[ship] = list(shipShellsSet)
-                            else:
-                                shipShells[ship] = list(ammoList)
-                                if len(ammoList) > 2:
-                                    print(ship)
-                            #print(shipShells[ship])
+    upgrades = attributes["ShipUpgradeInfo"]
+    artilleryNames = set()
+    shellGroups = set()
+    for upgradeName, upgradeInfo in upgrades.items():
+        if type(upgradeInfo) is dict:
+            if 'artillery' in upgradeInfo['components']:
+                for artilleryName in upgradeInfo['components']['artillery']:
+                    artilleryNames.add(artilleryName)
+
+    shells = set()
+    for artilleryName in artilleryNames:
+        artilleryConfig = attributes[artilleryName]
+        for turrets, turretValues in artilleryConfig.items():
+            if type(turretValues) is dict:
+                if 'typeinfo' in turretValues:
+                    if turretValues['typeinfo']['species'] == 'Main':
+                        shellGroup = ()
+                        for shell in turretValues['ammoList']:
+                            added = True
+                            shells.add(shell)
+                            shellGroup += shell,
+                        shellGroups.add(shellGroup)
+    
+    shipShells[ship] = list(shells)
     if added:
         temp = {}
+        tempEssential = {}
         for shell in shipShells[ship]:
-            temp[shell] = condenseShell[shell]
-        shipShellData[ship] = temp
+            ammoType = condenseShell[shell]['ammoType']
+            if not ammoType in temp:
+                temp[ammoType] = {}
+                tempEssential[ammoType] = {}
+            temp[ammoType][shell] = condenseShell[shell]
+            tempEssential[ammoType][shell] = selectEssential(condenseShell[shell])
+        
+        temp['Tier'] = attributes['level']
+        temp['Nation'] = attributes['typeinfo']['nation']
+        temp['ShellGroups'] = list(shellGroups)
 
-writeToFile(shipShells, '', 'shipShells.json')
-writeToFile(shipShellData, '', 'shipShellData.json')            
+        tempEssential['Tier'] = attributes['level']
+        tempEssential['Nation'] = attributes['typeinfo']['nation']
+        tempEssential['ShellGroups'] = list(shellGroups)
+
+        shipShellData[ship] = temp
+        shipShellDataEssential[ship] = tempEssential
+
+writeShipShells = True
+shipShellsDirectory = 'shipShells'
+if writeShipShells:
+    checkMakeDir(shipShellsDirectory)
+    writeToFile(shipShells, shipShellsDirectory, 'shipShells.json')
+    writeToFile(shipShellData, shipShellsDirectory, 'shipShellData.json')
+    writeToFile(shipShellDataEssential, shipShellsDirectory, 'shipShellDataEssentials.json')            
 
 
     
